dbe28b981a0f2377c3e21cd28bc8e48a823db9c3
homutov-sergey <84271546+homutov-sergey@users.noreply.github.com>
2021-10-24 00:59:32 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.welcom-slide').slick({
+    respondTo: 'slider',
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    autoplay: false,
+    fade: true,
+    cssEase: 'linear'
+  });
